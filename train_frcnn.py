b2f6543616814fc5bb413339a08ed0d2cbfa6619
JonasHarnau <jonas.harnau@economics.oxon.org>
2017-09-08 12:32:33 +0100

    fixed get_validation_loss import

--- train_frcnn.py
+++ train_frcnn.py
@@ -15,7 +15,7 @@ from keras_frcnn import config, data_generators
 from keras_frcnn import losses as losses
 import keras_frcnn.roi_helpers as roi_helpers
 from keras.utils import generic_utils
-import keras_frcnn.get_validation_loss as get_validation_loss
+from keras_frcnn.get_validation_loss import get_validation_loss
 
 sys.setrecursionlimit(40000)
 
